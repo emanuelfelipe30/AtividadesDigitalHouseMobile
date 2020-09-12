0753a52d1593fb49b7e25e83744572104e1325db
emanuel tavares <emanuelfelipe30@gmail.com>
2020-09-12 12:45:32 -0300

    deletando arquivos antigos

--- 02 - Exercícios Kotlin/Exercício 1 - Sequencial/src/nivel1/Exercicio3.kt
+++
@@ -1,9 +0,0 @@
-package nivel1
-
-fun main(){
-    print("Digite um valor em metros: ")
-    val valorMetros = readLine()!!.toInt()
-
-    val valorCentimetros = valorMetros * 100
-    println("$valorMetros metros é igual a $valorCentimetros centímetros")
-}
--- 02 - Exercícios Kotlin/Exercício 1 - Sequencial/src/nivel2/Exercicio1.kt
+++
@@ -1,9 +0,0 @@
-package nivel2
-
-fun main(){
-    print("Digite a temperatura em graus Fahrenheit: ")
-    val temperaturaFahrenheit = readLine()!!.toDouble()
-
-    val temperaturaCelsius = 5 * ((temperaturaFahrenheit-32) / 9)
-    println("a temperatura em graus celsius é $temperaturaCelsius ºC")
-}
